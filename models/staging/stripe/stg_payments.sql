b12438887d80151d59eccd99c3242b21c19211d3
Kelly Anderson <kanderson@connexionpoint.com>
2022-10-12 20:42:04 +0000

    fundamentals course finish

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,13 +1,9 @@
-with payments as (
-
-    select
-        orderid as order_id,
-        created as order_date,
-        status,
-        amount
-
-    from dev_kelly.stripe.payment
-
-)
-
-select * from payments
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+from {{ source('stripe', 'payment') }}
